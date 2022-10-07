b27e96348f16207ab074a8553bd9103e70b82ace
ylian <ylian@172.29.4.110>
2022-10-07 17:06:42 -0300

    Agregue todo lo que faltaba menos el metodo de Ranking.

--- src/interfaz/Sistema.java
+++ src/interfaz/Sistema.java
@@ -8,6 +8,7 @@ import dominio.Jugador;
 import dominio.ListaJugadores;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -15,119 +16,246 @@ import java.util.Scanner;
  */
 public class Sistema {
     private static ListaJugadores Jugadores = new ListaJugadores();
-    private static String configTablero = "Standard";
+    private static String ConfigTablero = "Standard";
     
-    public void menuInicial() {
-       System.out.println("********************************\n"
-        + "a. Registrar jugador\n"
-        + "b. Establecer tablero\n"
-        + "c. Jugar partida\n"
-        + "d. Ver ranking\n"
-        + "e. Salir\n"
-        + "Escribe una de las opciones.\n"
-        + "********************************\n");
-   }
-    
-    
-    public String[] pedirJugador() {
-        String[] datos = new String[3];
-        
-        Scanner in = new Scanner(System.in);
-        
-        System.out.println("ingrese nombre del jugador");
-        datos[0] = in.nextLine();
-        
-        System.out.println("ingrese edad del jugador");
-        datos[1] = in.nextLine();
-        
-        System.out.println("ingrese alias del jugador");
-        datos[2] = in.nextLine();
-        
-        return datos;
+    public static void main(String[] args) {
+        menuInicial();
     }
     
-    private void registrarJugador(String nombre, String edad, String alias) {
+    //METODOS DE MENU
+    public static void menuInicial() {
+       System.out.println("********************************\n"
+            + " BIENVENIDO AL JUEGO DISTANCIA\n"
+            + "********************************\n"
+            + "a. Registrar jugador\n"
+            + "b. Establecer tablero\n"
+            + "c. Jugar partida\n"
+            + "d. Ver ranking\n"
+            + "e. Salir del menu\n"
+            + "Escribe una de las opciones:\n"
+            + "********************************\n");
+       
+        char opcion = leerOpcionMenu('a', 'e');
+        opciones(opcion);      
+   }
+    
+   public static void opciones(char opcion) {
+       opcion = Character.toLowerCase(opcion);
+       switch(opcion){
+            case 'a' -> {
+                System.out.println("Has seleccionado Registrar jugador ");
+                String datos[] = pedirJugador();
+                registrarJugador(datos[0], datos[1], datos[2]);
+                volverAlMenu();
+            }
+            case 'b' -> {
+                System.out.println("Has seleccionado Establecer tablero");
+                String tipo = pedirTipoTablero();
+                establecerTablero(tipo);
+                volverAlMenu();
+            }
+             case 'c' -> {
+                System.out.println("Has seleccionado Jugar partida");
+                jugarPartida();
+            }
+             case 'd' -> {
+                System.out.println("Has seleccionado Ranking");
+                verRanking();
+                volverAlMenu();
+            }
+             case 'e' -> System.out.println("Gracias por usar el programa.");
+        }
+    }
+   
+   public static void volverAlMenu() {
+       System.out.println("********************************\n"
+               + "Desea volver al menu principal? \n"
+               + "a. Si. \n"
+               + "b. No. \n"
+               + "********************************\n");
+       
+       Scanner in = new Scanner(System.in);
+       
+       char opcion = leerOpcionMenu('a', 'b');
+       
+       switch (opcion) {
+           case 'a' -> menuInicial();
+           case 'b' -> System.out.println("********************************\n"
+                       + " Gracias por usar el programa. \n "
+                       + "********************************\n");
+       }  
+   }
+   
+   //METODOS DE OPCION
+    private static void establecerTablero(String tipo) {
+        ConfigTablero = tipo;
+        System.out.println("Se ha seleccionado " +tipo +" como tipo de tablero.");
+    }
+   
+    private static void registrarJugador(String nombre, String edad, String alias) {
         int edadNumerica = Character.getNumericValue(edad.charAt(0));
         Jugador jugador = new Jugador(nombre, edadNumerica, alias);
         Jugadores.agregarJugador(jugador);
+        System.out.println("El jugador " +alias + " ha sido registrado correctamente.");
     }
     
-    public String pedirTipoTablero() {
+    private static void jugarPartida() {
+       Partida nueva = new Partida(ConfigTablero, Jugadores);
+       nueva.iniciarPartida();
+    }
+   
+    private static void verRanking() {
+       //MOSTRAR RANKING
+    }
+    
+    //PETICIONES AL USUARIO
+    private static String pedirTipoTablero() {
         Scanner in = new Scanner(System.in);
         
         String tipo = "";
         
         System.out.println("Ingrese el tablero a usar: \n"
-                            + "1. Standard\n" 
-                            + "2. Precargado 1\n"
-                            + "3. Precargado 2\n"
+                            + "a. Standard\n" 
+                            + "b. Precargado 1\n"
+                            + "c. Precargado 2\n"
                             + "Elija una de las opciones.\n"
                             + "********************************\n");
         
-        int opcion = leerOpcionMenu(1, 3);
+        char opcion = leerOpcionMenu('a', 'c');
         
         tipo = switch (opcion) {
-            case 2 -> "Precargado 1";
-            case 3 -> "Precargado 2";
+            case 'b' -> "Precargado 1";
+            case 'c' -> "Precargado 2";
             default -> "Standard";
         };
         
         return tipo;
     }
     
-    public int leerOpcionMenu(int min, int max) {
+    private static String[] pedirJugador() {
+        String[] datos = new String[3];
+        
         Scanner in = new Scanner(System.in);
         
-        String txtErrorRango = "Debe ingresar un numero entre el " +min +" y el " +max +". Por favor ingrese un valor correcto: ";
+        System.out.println("Ingrese nombre del jugador:");
+        datos[0] = leerNombreJugador();
         
-        int opcion = 0;
-        boolean x = true;
-        while (x) {
+        System.out.println("Ingrese edad del jugador:");
+        datos[1] = String.valueOf(leerEdadJugador());
+        
+        System.out.println("Ingrese alias del jugador:");
+        datos[2] = leerAliasJugador();
+        
+        return datos;
+    }
+    
+    //LECTURA POR PANTALLA Y MANEJO DE EXCEPCIONES
+    private static char leerOpcionMenu(char min, char max) {
+        Scanner in = new Scanner(System.in);
+        
+        String txtErrorRango = "Debe ingresar una letra entre " +min +" y " +max +". \n"
+                + "Por favor ingrese un valor correcto: ";
+        
+        char opcion = 0;
+        boolean opcionInvalida = true;
+        
+        while (opcionInvalida) {
             try {
-                opcion = in.nextInt();
-                if (opcion <= max && opcion >= min) {
-                    x = false;
+                String strOpcion = in.nextLine();
+                opcion = Character.toLowerCase(strOpcion.charAt(0));
+                
+                if (opcion <= max && opcion >= min && strOpcion.length() == 1) {
+                    opcionInvalida = false;
                 } else {
                     System.out.println(txtErrorRango);
                 }
             } catch (InputMismatchException e){
-                System.out.println("Debe ingresar un numero natural. Por favor ingrese un valor correcto: ");
+                System.out.println("Debe ingresar una letra. \n"
+                        + "Por favor ingrese un valor correcto: ");
                 in.nextLine();
             }
         }
-        
         return opcion;
     }
-       
     
-    private void setConfigTablero(String tipo) {
-        configTablero = tipo;
-    }
+    private static String leerNombreJugador() {
+        Scanner in = new Scanner(System.in);
         
-   public void mostrarOpcion(char opcion) {
-       switch(opcion){
-            case 1:
-                System.out.println("Has seleccionado Registrar jugador ");
-                String datos[] = pedirJugador();
-                registrarJugador(datos[0], datos[1], datos[2]);
-                break;
-            case 2:
-                System.out.println("Has seleccionado Establecer tablero");
-                String tipo = pedirTipoTablero();
-                setConfigTablero(tipo);
-                break;
-             case 3:
-                System.out.println("Has seleccionado Jugar partida");
-                break;
-             case 4:
-                System.out.println("Has seleccionado Ranking");
-                break;
-             case 5:
-                System.out.println("Gracias por usar el programa.");
-                break;
+        String txtErrorCaracteres = "El nombre puede incluir unicamente letras de la A a la Z. \n"
+                + "Por favor ingrese un nombre correcto: ";
+        
+        String opcion = "";
+        boolean opcionInvalida = true;
+        
+        while (opcionInvalida) {
+            try {
+                opcion = in.nextLine();
+                if (!Pattern.matches("[a-zA-Z]+", opcion))  {
+                    System.out.println(txtErrorCaracteres);
+                } else {
+                    opcionInvalida = false;
+                }
+            } catch (InputMismatchException e){
+                System.out.println("Por favor, ingrese un nombre valido: \n");
+                in.nextLine();
+            }
         }
-
+        return opcion;
     }
     
+    private static String leerAliasJugador() {
+        Scanner in = new Scanner(System.in);
+        
+        String txtErrorCaracteres = "El alias puede incluir unicamente letras minusculas de la A a la Z. \n"
+                + "Por favor ingrese un alias correcto: ";
+        String txtErrorAliasExistente = "El alias ya esta en uso. \n"
+                + "Por favor ingrese un alias que este disponible:";
+        
+        String opcion = "";
+        boolean opcionInvalida = true;
+        
+        while (opcionInvalida) {
+            try {
+                opcion = in.nextLine();
+                if (!Pattern.matches("[a-z]+", opcion))  {
+                    System.out.println(txtErrorCaracteres);
+                } else if (Jugadores.existeAlias(opcion))  {
+                    System.out.println(txtErrorAliasExistente);
+                } else {
+                    opcionInvalida = false;
+                }
+            } catch (InputMismatchException e){
+                System.out.println(txtErrorCaracteres);
+                in.nextLine();
+            }
+        }
+        return opcion;
+    }
     
+    private static int leerEdadJugador() {
+        Scanner in = new Scanner(System.in);
+        
+        String txtErrorRango = "Debe ingresar un entero entre 1 y 100. \n"
+                + "Por favor ingrese un valor correcto: ";
+        
+        int opcion = 0;
+        boolean opcionInvalida = true;
+        
+        while (opcionInvalida) {
+            try {
+                opcion = in.nextInt();
+                
+                if (opcion <= 100 && opcion >= 1) {
+                    opcionInvalida = false;
+                } else {
+                    System.out.println(txtErrorRango);
+                }
+            } catch (InputMismatchException e){
+                System.out.println("Debe ingresar un numero natural. \n"
+                        + "Por favor ingrese un valor correcto: ");
+                in.nextLine();
+            }
+        }
+        return opcion;
+    }  
 }
