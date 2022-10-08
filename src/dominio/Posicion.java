f550d263720b89b75c996f97b5118e7cda25249f
ylian <ylian@172.29.4.110>
2022-10-08 13:52:18 -0300

    Agregue descripcion de la clase y headers. Reordene las variables y los metodos segun jerarquia. Cambie la firma del constructor: ahora es independiente del tablero.

--- src/dominio/Posicion.java
+++ src/dominio/Posicion.java
@@ -1,76 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/* Una Posicion representa un lugar puntual de un Tablero cualquiera.
+ * El valor de esta clase reside en la información de distancias al centro.
  */
 package dominio;
-
-import java.util.Arrays;
 
 /**
  *
  * @author ylian
  */
 public class Posicion {
+    
+    private static final int[][] PosConDistanciaUno = {{2, 2}, {2, 3}, {3, 2}, {3, 3}};
+    private static final int[][] PosConDistanciaDos = {{1, 2}, {1, 3}, {2, 1}, {2, 4},
+                                                      {3, 1}, {3, 4}, {4, 2}, {4, 3}};                                           
+    private static final int[][] PosConDistanciaTres = {{1, 1}, {1, 4}, {5, 1}, {5, 4}};
+    private static final int[][] PosConDistanciaCuatro = {{0, 2}, {0, 3}, {2, 0}, {2, 3},
+                                                         {2, 5}, {3, 5}, {5, 2}, {5, 3}};
+    private static final int[][] PosConDistanciaCinco = {{0, 1}, {0, 4}, {1, 0}, {1, 5},
+                                                        {4, 0}, {4, 5}, {5, 1}, {5, 4}};
+    private static final int[][] PosConDistanciaSeis = {{0, 0}, {0, 5}, {5, 0}, {5,5}};
+    
     private int fila;
     private int col;
     private int distanciaAlCentro;
-    
-    private static int[][] posConDistanciaUno = {{2, 2}, {2, 3}, {3, 2}, {3, 3}};
 
-    private static int[][] posConDistanciaDos = {{1, 2}, {1, 3}, {2, 1}, {2, 4},
-                                                 {3, 1}, {3, 4}, {4, 2}, {4, 3}};
-                                                  
-    private static int[][] posConDistanciaTres = {{1, 1}, {1, 4}, {5, 1}, {5, 4}};
-
-    private static int[][] posConDistanciaCuatro = {{0, 2}, {0, 3}, {2, 0}, {2, 3},
-                                                    {2, 5}, {3, 5}, {5, 2}, {5, 3}};
-   
-    private static int[][] posConDistanciaCinco = {{0, 1}, {0, 4}, {1, 0}, {1, 5},
-                                                   {4, 0}, {4, 5}, {5, 1}, {5, 4}};
-    
-    private static int[][] posConDistanciaSeis = {{0, 0}, {0, 5}, {5, 0}, {5,5}};
-    
-    //CONSTRUCTORES
-    public Posicion(int fila, int col, Tablero unTablero) {
+    /*CONSTRUCTORES*/
+    public Posicion(int fila, int col) {
         this.fila = fila;
         this.col = col;
         this.distanciaAlCentro = calcularDistanciaAlCentro(fila, col);
     }
     
-    //METODOS DE ACCESO
-    public int getDistanciaAlCentro() {
-        return this.distanciaAlCentro;
-    }
-    
-    public int getFila() {
-        return this.fila;
-    }
-    
-    public int getCol() {
-        return this.col;
-    }
-    
-    public char getFichaEnPos(Tablero unTablero) {
-        return unTablero.getElem(this.fila, this.col);
-    }
-    
+    /*METODOS AUXILIARES*/
     private int calcularDistanciaAlCentro(int fila, int col) {
-        int distancia = 0;
-        String pos = fila + "-" + col;
+        int distancia;
         
-        if (estaEnLaMatriz(fila, col, posConDistanciaUno)) {
+        if (estaEnLaMatriz(fila, col, PosConDistanciaUno)) {
           distancia = 1;  
           
-        } else if (estaEnLaMatriz(fila, col, posConDistanciaDos)) {
+        } else if (estaEnLaMatriz(fila, col, PosConDistanciaDos)) {
           distancia = 2;  
           
-        } else if (estaEnLaMatriz(fila, col, posConDistanciaTres)) {
+        } else if (estaEnLaMatriz(fila, col, PosConDistanciaTres)) {
           distancia = 3;  
           
-        } else if (estaEnLaMatriz(fila, col, posConDistanciaCuatro)) {
+        } else if (estaEnLaMatriz(fila, col, PosConDistanciaCuatro)) {
           distancia = 4;  
           
-        } else if (estaEnLaMatriz(fila, col, posConDistanciaCinco)){
+        } else if (estaEnLaMatriz(fila, col, PosConDistanciaCinco)){
             distancia = 5;
             
         } else {
@@ -92,9 +68,20 @@ public class Posicion {
         return esta;
     }
     
-    @Override 
-    public String toString() {
-        return this.getFila() + "-" + this.getCol();
+    /*METODOS DE ACCESO*/
+    public int getFila() {
+        return this.fila;
     }
     
+    public int getCol() {
+        return this.col;
+    }
+    
+    public int getDistanciaAlCentro() {
+        return this.distanciaAlCentro;
+    }
+    
+    public char getFichaEnPos(Tablero unTablero) {
+        return unTablero.getFicha(this.fila, this.col);
+    }
 }
