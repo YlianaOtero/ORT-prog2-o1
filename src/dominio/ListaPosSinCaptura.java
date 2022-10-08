9db228c7a69818d4d37f78269b53eb5338fd65d1
ylian <ylian@172.29.4.110>
2022-10-08 15:15:56 -0300

    Agregue descripcion de la clase y headers. Reordene las variables y los metodos segun jerarquia. Mejore los metodos para que se aprovechen lo mas posible los atributos. Limpie un poco el codigo.

--- src/dominio/ListaPosSinCaptura.java
+++ src/dominio/ListaPosSinCaptura.java
@@ -1,7 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/* ListaPosSinCaptura se encarga de la lógica de los movimientos sin captura del
+ * juego. Un objeto de esta clase tiene una posición a partir de la cual se genera
+ * la lista de posiciones a las cuales se podría desplazar sin captura, siempre
+ * dentro de un mismo tablero dado.*/
 package dominio;
 
 import java.util.ArrayList;
@@ -11,99 +11,90 @@ import java.util.ArrayList;
  * @author ylian
  */
 public class ListaPosSinCaptura {
+
     private Posicion inicio;
+    private Tablero tablero;
     private ArrayList<Posicion> lista;
-    
+
+    /*CONSTRUCTORES*/
     public ListaPosSinCaptura(Posicion unaPosicion, Tablero unTablero) {
         this.inicio = unaPosicion;
-        this.lista = listarPosiciones(unaPosicion, unTablero);
+        this.tablero = unTablero;
+        this.lista = listarPosiciones();
     }
-    
-    private ArrayList<Posicion> listarPosiciones(Posicion unaPosicion, Tablero unTablero) {
-        ArrayList<Posicion> posiciones = listarPosHorizontal(unaPosicion, unTablero);
-        posiciones.addAll(listarPosVertical(unaPosicion, unTablero));
-        posiciones.addAll(listarPosDiagonal(unaPosicion, unTablero));
-        
+
+    /*METODOS DE MODIFICACION*/
+    private ArrayList<Posicion> listarPosiciones() {
+        ArrayList<Posicion> posiciones = listarPosHorizontal();
+        posiciones.addAll(listarPosVertical());
+        posiciones.addAll(listarPosDiagonal());
+
         return posiciones;
     }
-    
-    public ArrayList<Posicion> listarPosHorizontal(Posicion unaPosicion, Tablero unTablero) {
-        int fila = unaPosicion.getFila();
-        int col = unaPosicion.getCol();
-        
+
+    /*METODOS AUXILIARES*/
+    private ArrayList<Posicion> listarPosHorizontal() {
+        int fila = this.inicio.getFila();
+        int col = this.inicio.getCol();
+
         ArrayList<Posicion> posiciones = new ArrayList<Posicion>();
-        
-        if (col > 0 && unTablero.esLugarVacio(fila, col-1)) {
-            Posicion nueva = new Posicion(fila, col-1, unTablero);
-            posiciones.add(nueva);
+
+        if (col > 0 && this.tablero.esLugarVacio(fila, col - 1)) {
+            enlistar(fila, col - 1, posiciones);
         }
-        
-        if (col < unTablero.getTamanio()-1 && unTablero.esLugarVacio(fila, col+1)) {
-            Posicion nueva = new Posicion(fila, col+1, unTablero);
-            posiciones.add(nueva);
+
+        if (col < this.tablero.getTamanio() - 1 && this.tablero.esLugarVacio(fila, col + 1)) {
+            enlistar(fila, col + 1, posiciones);
         }
-        
+
         return posiciones;
     }
-    
-    private ArrayList<Posicion> listarPosVertical(Posicion unaPosicion, Tablero unTablero) {
-        int fila = unaPosicion.getFila();
-        int col = unaPosicion.getCol();
-        
+
+    private ArrayList<Posicion> listarPosVertical() {
+        int fila = this.inicio.getFila();
+        int col = this.inicio.getCol();
+
         ArrayList<Posicion> posiciones = new ArrayList<Posicion>();
-        
-        if (fila > 0 && unTablero.esLugarVacio(fila-1, col)) {
-            Posicion nueva = new Posicion(fila-1, col, unTablero);
-            posiciones.add(nueva);
+
+        if (fila > 0 && this.tablero.esLugarVacio(fila - 1, col)) {
+            enlistar(fila - 1, col, posiciones);
         }
-        if (fila < unTablero.getTamanio()-1 && unTablero.esLugarVacio(fila+1, col)) {
-            Posicion nueva = new Posicion(fila+1, col, unTablero);
-            posiciones.add(nueva);
+        if (fila < this.tablero.getTamanio() - 1 && this.tablero.esLugarVacio(fila + 1, col)) {
+            enlistar(fila + 1, col, posiciones);
         }
-        
+
         return posiciones;
     }
-    
-    private ArrayList<Posicion> listarPosDiagonal(Posicion unaPosicion, Tablero unTablero) {
-        int fila = unaPosicion.getFila();
-        int col = unaPosicion.getCol();
-        
+
+    private ArrayList<Posicion> listarPosDiagonal() {
+        int fila = this.inicio.getFila();
+        int col = this.inicio.getCol();
+
         ArrayList<Posicion> posiciones = new ArrayList<Posicion>();
-        
+
         if (fila > 0) {
-            if (col > 0 && unTablero.esLugarVacio(fila-1, col-1)) {
-                Posicion nueva = new Posicion(fila-1, col-1, unTablero);
-                posiciones.add(nueva);
+            if (col > 0 && this.tablero.esLugarVacio(fila - 1, col - 1)) {
+                enlistar(fila - 1, col - 1, posiciones);
             }
-            if (col < unTablero.getTamanio()-1 && unTablero.esLugarVacio(fila-1, col+1)) {
-                Posicion nueva = new Posicion(fila-1, col+1, unTablero);
-                posiciones.add(nueva);
+            if (col < this.tablero.getTamanio() - 1 && this.tablero.esLugarVacio(fila - 1, col + 1)) {
+                enlistar(fila - 1, col + 1, posiciones);
             }
         }
-        
-        if (fila < unTablero.getTamanio()-1) {
-            if (col > 0 && unTablero.esLugarVacio(fila+1, col-1)) {
-                Posicion nueva = new Posicion(fila+1, col-1, unTablero);
-                posiciones.add(nueva);
+
+        if (fila < this.tablero.getTamanio() - 1) {
+            if (col > 0 && this.tablero.esLugarVacio(fila + 1, col - 1)) {
+                enlistar(fila + 1, col - 1, posiciones);
             }
-            if (col < unTablero.getTamanio()-1 && unTablero.esLugarVacio(fila+1, col+1)) {
-                Posicion nueva = new Posicion(fila+1, col+1, unTablero);
-                posiciones.add(nueva);
+            if (col < this.tablero.getTamanio() - 1 && this.tablero.esLugarVacio(fila + 1, col + 1)) {
+                enlistar(fila + 1, col + 1, posiciones);
             }
         }
-        
+
         return posiciones;
     }
     
-    @Override 
-    public String toString() {
-        String lista = "";
-        
-        for (int i = 0; i < this.lista.size(); i++) {
-            Posicion elem = this.lista.get(i);
-            lista += elem.getFila() + "-" + elem.getCol() + " | ";
-        }
-        
-        return lista;
+    private void enlistar(int fila, int col, ArrayList<Posicion> lista) {
+        Posicion nueva = new Posicion(fila, col);
+        lista.add(nueva);
     }
 }
