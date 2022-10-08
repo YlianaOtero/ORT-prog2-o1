ff050cd95be25d65cfef6b8ccbfa00ac34111474
ylian <ylian@172.29.4.110>
2022-10-08 15:46:49 -0300

    Agregue descripcion de la clase y headers. Reordene las variables y los metodos segun jerarquia.

--- src/interfaz/Color.java
+++ src/interfaz/Color.java
@@ -1,33 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/* Color permite utilizar los codigos de colores ANSI que require el juego. Si se hace un
+ * System.out.print(CODIGO_ANSI + "Texto a imprimir"); obtendremos "Texto a imprimir"
+ * pintado del color correspondiente a ese codigo. */
 package interfaz;
 
 /**
  *
  * @author ylian
- * 
- * Los codigos de los colores son los codigos ANSI.
- * 
- * Si se hace System.out.print(CODIGO_ANSI + "Texto a imprimir"),
- * deberia mostrarse en pantalla el texto entre comillas, pintado del color
- * correspondiente a ese codigo.
  */
 public class Color {
+
     private String codigo;
     private String nombre;
-    
+
     private static String Negro = "\u001B[30m";
     private static String Rojo = "\u001B[31m";
     private static String Azul = "\u001B[34m";
     private static String Verde = "\u001b[32;1m";
-    
+
+    /*CONSTRUCTORES*/
     public Color() {
         this.codigo = Negro;
         this.nombre = "negro";
     }
+
+    /*METODOS DE MODIFICACION*/
+    public String getCodigo() {
+        return this.codigo;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
     
+    /*METODOS DE ACCESO*/
     public void setColor(String unColor) {
         if (unColor.equalsIgnoreCase("rojo")) {
             this.codigo = Rojo;
@@ -35,7 +40,7 @@ public class Color {
         } else if (unColor.equalsIgnoreCase("azul")) {
             this.codigo = Azul;
             this.nombre = "azul";
-        } else if (unColor.equalsIgnoreCase("verde")){
+        } else if (unColor.equalsIgnoreCase("verde")) {
             this.codigo = Verde;
             this.nombre = "verde";
         } else {
@@ -43,14 +48,4 @@ public class Color {
             this.nombre = "negro";
         }
     }
-    
-    public String getCodigo() {
-        return this.codigo;
-    }
-    
-    public String getNombre() {
-        return this.nombre;
-    }
-    
-    
 }
