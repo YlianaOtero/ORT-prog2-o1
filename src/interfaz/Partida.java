70cb157634538b658eead37fbc01cb47271d14be
Yliana <66150659+YlianaOtero@users.noreply.github.com>
2022-10-10 19:37:46 -0300

    Agregue muchos metodos
    Creo que no se necesitan mas metodos, pero hay algo que no funciona.
    Si bien no da errores, no esta comiendo bien las fichas...
    Cuando una ficha deberia comer a otra, en realidad no pone eso en el tablero, por ej si una ficha roja come a una ficha azul, no reemplaza 'A' por 'R'.

--- src/interfaz/Partida.java
+++ src/interfaz/Partida.java
@@ -1,10 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/* Representa una partida puntual del juego Distancia. */
 package interfaz;
 
 import dominio.Jugador;
+import dominio.ListaPosConCaptura;
+import dominio.ListaPosSinCaptura;
+import dominio.Posicion;
 import dominio.Tablero;
 
 /**
@@ -12,13 +12,10 @@ import dominio.Tablero;
  * @author ylian
  */
 public class Partida {
-
-    private static String Filas = "    1 2 3 4 5 6    ";
-    private static String Separador = "   +-+-+-+-+-+-+   ";
-    
-    private Jugador jugador1;
-    private Jugador jugador2;
+    private final Jugador jugador1; //Rojo
+    private final Jugador jugador2; //Azul
     private Tablero tablero;
+    private Jugador ganador;
 
     /*CONSTRUCTORES*/
     public Partida(String configTablero, Jugador[] jugadores) {
@@ -26,73 +23,188 @@ public class Partida {
         this.jugador2 = jugadores[1];
         this.tablero = new Tablero(configTablero);
     }
+    
+    /*METODOS DE ACCESO*/
+    public Jugador getGanador() {
+        return this.ganador;
+    }
 
-    public void iniciarPartida() {
-        //FALTA PROGRAMAR ESTE METODO
+    public Tablero getTablero() {
+        return this.tablero;
     }
     
-    private void mostrarTableroActual() {
-        Color negro = new Color();
-
-        String impresion = Filas + "\n" + Separador + "\n";
-
-        for (int i = 0; i < tablero.getTamanio(); i++) {
-            String letraBorde = letraBordeTablero(i);
-            impresion += " " + letraBorde + " |";
-
-            for (int j = 0; j < tablero.getTamanio(); j++) {
-                char ficha = tablero.getTablero()[i][j];
-                Color color = colorCorrespondiente(ficha);
-                String coloreada = letraPintada(ficha, color);
-
-                impresion += coloreada + negro.getCodigo() +"|";
+    /*METODOS DE MODIFICACION*/
+    private void setGanador() {
+        if (tablero.getCantFichasAzules() <= 0) {
+            this.ganador = jugador1;
+        } else {
+            this.ganador = jugador2;
+        }
+    }
+    
+    /*BASICOS DEL JUEGO*/
+    public void iniciarPartida() {
+        int valor = jugada(jugador1);
+        Jugador ultimo = jugador1;
+        
+        while (this.tablero.getCantFichasAzules() > 0 && this.tablero.getCantFichasRojas() > 0 
+                && valor != -1)  {
+            if (ultimo == jugador1) {
+                valor = jugada(jugador2);
+                ultimo = jugador2;
+            } else {
+                valor = jugada(jugador1);
+                ultimo = jugador1;
             }
+        }
+        
+        if (valor != -1) {
+            setGanador();
+        } 
+        
+        finPartida();
+    }
+    
+    private void finPartida() {
+        System.out.println("Fin de la partida! \n"
+                + "El ganador es " +this.ganador.getAlias());
+    }
+    
+    private int jugada(Jugador unJugador) {
+        int valor = 0;
 
-            impresion += " " + negro.getCodigo() + letraBorde + "\n" + Separador + "\n";
+        if (hayJugadasPosiblesGral()) {
+            imprimirHeaderJugada(unJugador);
+            if (!hayJugadasPosibles(unJugador)) {
+            System.out.println("No hay jugadas posibles para este jugador. \n"
+                    + "Se pasa el turno.");
+            } else {
+                Posicion posInicial = movimientoInicial(unJugador);
+                valor = movimientoFinal(unJugador, posInicial);
+            }
         }
 
-        impresion += Filas;
-        System.out.println(impresion);
+        return valor;
     }
-
-    private Color colorCorrespondiente(char ficha) {
-        Color colorCorrespondiente = new Color();
-
-        switch (ficha) {
-            case 'A' ->
-                colorCorrespondiente.setColor("azul");
-            case 'R' ->
-                colorCorrespondiente.setColor("rojo");
+    
+    private void imprimirHeaderJugada(Jugador unJugador) {
+        String colorFicha = "rojas";
+        
+        if (!esJugador1(unJugador)) {
+            colorFicha = "azules";
         }
-
-        return colorCorrespondiente;
+        
+        System.out.println("Juega " +unJugador.getAlias() +" con las fichas " 
+                +colorFicha + "\n");
+        DisplayTablero.mostrarTableroActual(this);
     }
-
-    private String letraPintada(char letra, Color color) {
-        String codigo = color.getCodigo();
-        return codigo + letra;
+    
+    private void rendicion(Jugador unJugador) {
+        System.out.println(unJugador.getAlias() +" se ha rendido!");
+        if (esJugador1(unJugador)) {
+            this.ganador = jugador2;
+        } else {
+            this.ganador = jugador1;
+        }
     }
+    
+    /*MOVIMIENTOS*/
+    private Posicion movimientoInicial(Jugador unJugador) {
+        Posicion pos = Lectura.leerPosicionInicial();
+        while (!esJugadaInicialCorrecta(pos, unJugador)) {
+            System.out.println("Por favor, ingrese una posicion que tenga una de sus fichas: ");
+            pos = Lectura.leerPosicionInicial();
+        }
+        
+        return pos;
+    }
+    
+    //Si retorna 0, se movio sin captura. Si retorna 1, se movio con captura. Si retorna -1, se rindio.
+    private int movimientoFinal(Jugador unJugador, Posicion posInicial) {
+        int retorno = 0;
+        
+        ListaPosConCaptura movsConCaptura = new ListaPosConCaptura(posInicial, this.tablero);
+        ListaPosSinCaptura movsSinCaptura = new ListaPosSinCaptura(posInicial, this.tablero);
+        
+        DisplayTablero.mostrarTableroPosibilidades(movsConCaptura, movsSinCaptura, posInicial, this);
 
-    private String letraBordeTablero(int fila) {
-        String letra;
-
-        letra = switch (fila) {
-            case 0 ->
-                "A";
-            case 1 ->
-                "B";
-            case 2 ->
-                "C";
-            case 3 ->
-                "D";
-            case 4 ->
-                "E";
-            case 5 ->
-                "F";
-            default ->
-                "";
-        };
-
-        return letra;
+        String posFinalString = Lectura.leerPosicionFinal();
+        if (posFinalString.equals("X")) {
+            rendicion(unJugador);
+            retorno = -1;
+        } else {
+            MapeoPosicion mapFinal = new MapeoPosicion(posFinalString);
+            if (!esJugadaFinalCorrecta(movsConCaptura, movsSinCaptura, mapFinal.getPosicion())) {
+                System.out.println("Ha elegido una posicion incorrecta. \n"
+                        + "Se ha reseteado la jugada");
+                jugada(unJugador);
+            } else if (retorno != -1) {
+                retorno = moverse(movsConCaptura, movsSinCaptura, posInicial, mapFinal.getPosicion());
+            }
+        }
+        
+        return retorno;
+    }
+    
+    private int moverse(ListaPosConCaptura movsConCaptura, ListaPosSinCaptura movsSinCaptura,
+                        Posicion inicio, Posicion fin) {
+        int retorno = 0;
+        
+        if (movsConCaptura.estaEnLaLista(fin)) {
+            tablero.comerFicha(inicio.getFila(), inicio.getCol(), fin.getFila(), fin.getCol());
+            retorno = 1;
+        } else if (movsSinCaptura.estaEnLaLista(fin)) {
+            tablero.desplazarFicha(inicio.getFila(), inicio.getCol(), fin.getFila(), fin.getCol());
+        }
+        
+        return retorno;
+    }
+    
+    /*PREDICADOS*/
+    private boolean hayJugadasPosibles(Jugador unJugador) {
+        boolean hay = false;
+        char ficha = 'R';
+        if (!esJugador1(unJugador)) {
+            ficha = 'A';
+        }  
+        
+        for (int i = 0; i < tablero.getTamanio() && !hay; i++) {
+            for (int j = 0; j < tablero.getTamanio() && !hay; j++) {
+                if (tablero.getFicha(i, j) == ficha) {
+                    Posicion pos = new Posicion(i, j);
+                    ListaPosConCaptura movsConCaptura = new ListaPosConCaptura(pos, tablero);
+                    ListaPosSinCaptura movsSinCaptura = new ListaPosSinCaptura(pos, tablero);
+                    if (movsConCaptura.hayMovimientos() || movsSinCaptura.hayMovimientos()) {
+                        hay = true;
+                    }
+                }
+            }
+        }
+     
+        return hay;
+    }
+    
+    private boolean hayJugadasPosiblesGral() {
+        return hayJugadasPosibles(jugador1) || hayJugadasPosibles(jugador2);
+    }
+  
+    private boolean esJugadaFinalCorrecta(ListaPosConCaptura movsConCaptura, 
+                                    ListaPosSinCaptura movsSinCaptura,
+                                    Posicion elegida) {
+        return movsConCaptura.estaEnLaLista(elegida) || movsSinCaptura.estaEnLaLista(elegida);
+    }
+    
+    private boolean esJugadaInicialCorrecta(Posicion elegida, Jugador unJugador) {
+        boolean respuesta = true;
+        if (esJugador1(unJugador)) {
+            respuesta = tablero.getTablero()[elegida.getFila()][elegida.getCol()] == 'R';
+        } else {
+            respuesta = tablero.getTablero()[elegida.getFila()][elegida.getCol()] == 'A';
+        }
+        return respuesta;
+    }
+    
+    private boolean esJugador1(Jugador unJugador) {
+        return unJugador.getAlias().equals(jugador1.getAlias());
     }
 }
