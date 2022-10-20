a3e335d8c7ff48f6c36911221fbf2b410752b577
Yliana <66150659+YlianaOtero@users.noreply.github.com>
2022-10-20 07:46:43 -0300

    Version entregada

--- src/interfaz/DisplayTablero.java
+++ src/interfaz/DisplayTablero.java
@@ -1,19 +1,24 @@
-/* DisplayTablero permite imprimir por pantalla los tableros de una partida dada,
-con el estado actual del tablero de dicha partida. */
 package interfaz;
 
 import dominio.ListaPosConCaptura;
+import dominio.ListaPosPosibles;
 import dominio.ListaPosSinCaptura;
 import dominio.Posicion;
 
-/**
- *
- * @author ylian
- */
+/** DisplayTablero permite imprimir por pantalla los tableros de una partida dada,
+ * con el estado actual del tablero de dicha partida.
+ * Es una clase abstracta porque no nos interesa crear objetos de tipo DisplayTablero, y
+ * sus metodos son static porque nos permite llamarlos desde otras clases sin necesidad
+ * de instanciar DisplayTablero en ellas.
+ * @author yliana*/
+
 public abstract class DisplayTablero {
     private static final String Filas = "    1 2 3 4 5 6    ";
     private static final String Separador = "   +-+-+-+-+-+-+   ";
     
+    /** Imprime por pantalla el tablero actual de una partida, con el formato 
+     * solicitado para el juego.
+     * @param unaPartida recibe la partida que contiene el tablero a mostrar.*/
     public static void mostrarTableroActual(Partida unaPartida) {
         Color negro = new Color();
 
@@ -38,6 +43,9 @@ public abstract class DisplayTablero {
         System.out.println(impresion);
     }
     
+    /** @param fila Recibe la fila del tablero.
+     * @return Devuelve un String con la letra a colocar en el borde de esa fila 
+     * del tablero cuando lo vamos a mostrar por pantalla. */
     private static String letraBordeTablero(int fila) {
         String letra;
 
@@ -60,7 +68,13 @@ public abstract class DisplayTablero {
 
         return letra;
     }
-    
+
+    /** Imprime por pantalla el tablero correspondiente a una posicion elegida 
+     * en una partida, con el formato solicitado para el juego.
+     * @param movsConCaptura Es la lista de todos los movimientos posibles con captura.
+     * @param movsSinCaptura Es la lista de todos los movimientos posibles sin captura.
+     * @param elegida Es la posicion elegida, desde la cual estamos "mirando" el tablero.
+     * @param unaPartida Recibe la partida que contiene el tablero a mostrar.*/
     public static void mostrarTableroPosibilidades(ListaPosConCaptura movsConCaptura, 
                                             ListaPosSinCaptura movsSinCaptura,
                                             Posicion elegida, Partida unaPartida) {
@@ -74,9 +88,9 @@ public abstract class DisplayTablero {
             for (int j = 0; j < unaPartida.getTablero().getTamanio(); j++) {
                 Posicion pos = new Posicion(i, j);
                 char ficha = unaPartida.getTablero().getTablero()[i][j];
-                if (movsConCaptura.estaEnLaLista(pos)) {
+                if (ListaPosPosibles.estaEnLaLista(pos, movsConCaptura.getLista())) {
                     ficha = '#';
-                } else if (movsSinCaptura.estaEnLaLista(pos)) {
+                } else if (ListaPosPosibles.estaEnLaLista(pos, movsSinCaptura.getLista())) {
                     ficha = '*';
                 } else if (elegida.getFila() == i && elegida.getCol() == j) {
                     ficha = 'E';
@@ -92,6 +106,5 @@ public abstract class DisplayTablero {
 
         impresion += Filas;
         System.out.println(impresion);
-        
     }
 }
