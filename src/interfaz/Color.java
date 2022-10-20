f4d8645ec52e21782bd1066f14b6260ccdfc900f
Yliana <66150659+YlianaOtero@users.noreply.github.com>
2022-10-20 07:46:15 -0300

    Version entregada

--- src/interfaz/Color.java
+++ src/interfaz/Color.java
@@ -1,12 +1,10 @@
-/* Color permite utilizar los codigos de colores ANSI que require el juego. Si se hace un
- * System.out.print(CODIGO_ANSI + "Texto a imprimir"); obtendremos "Texto a imprimir"
- * pintado del color correspondiente a ese codigo. */
 package interfaz;
 
-/**
- *
- * @author ylian
- */
+/**Color permite utilizar los codigos de colores ANSI que require el juego. Si se hace un
+ * System.out.print(CODIGO_ANSI + "Texto a imprimir"); obtendremos "Texto a imprimir"
+ * pintado del color correspondiente a ese codigo.
+ * @author yliana*/
+
 public class Color {
 
     private String codigo;
@@ -18,17 +16,20 @@ public class Color {
     private static String Azul = "\u001B[34m";
     private static String Verde = "\u001b[32;1m";
 
-    /*CONSTRUCTORES*/
+    /** Constructor por defecto.*/
     public Color() {
         this.codigo = Reset;
         this.nombre = "";
     }
     
-    public Color (char letra) {
-        switch (letra) {
+    /** Constructor.
+     * @param caracter Si caracter es 'A', settea el color a azul. 
+     * Si es 'R', settea el color a rojo. En cualquier otro caso, lo settea a verde.*/
+    public Color(char caracter) {
+        switch (caracter) {
             case 'A' -> {
                this.codigo = Azul;
-                this.nombre = "azul"; 
+               this.nombre = "azul"; 
             }
             case 'R' -> {
                 this.codigo = Rojo;
@@ -41,16 +42,19 @@ public class Color {
         }
     }
 
-    /*METODOS DE ACCESO*/
+    /** @return Devuelve el codigo ANSI del color correspondiente.*/
     public String getCodigo() {
         return this.codigo;
     }
 
+    /** @return Devuelve el nombre del color correspondiente.*/
     public String getNombre() {
         return this.nombre;
     }
-    
-    /*METODOS DE MODIFICACION*/
+
+    /** Modifica los atributos nombre y codigo de un objeto de tipo Color.
+     * @param unColor Recibe el nombre de un color. Si recibe rojo, azul o verde,
+     * settea el color correspondiente. En cualquier otro caso, settea el color negro.*/
     public void setColor(String unColor) {
         if (unColor.equalsIgnoreCase("rojo")) {
             this.codigo = Rojo;
@@ -67,6 +71,10 @@ public class Color {
         }
     }
 
+    /**"Pinta" una letra (o simbolo), apoyandose en el codigo ANSI del color.
+     * @param letra Es el caracter a "pintar".
+     * @return Devuelve un String del formato CODIGOletra. Si imprimimos esto por
+     * pantalla, deberia mostrarse la variable letra en el color correspondiente.*/
     public String letraPintada(char letra) {
         String codigo = this.getCodigo();
         return codigo + letra;
