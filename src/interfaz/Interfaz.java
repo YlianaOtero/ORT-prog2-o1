61a962e06994e5b7cadc7f676cab13cca5dacf49
Yliana <66150659+YlianaOtero@users.noreply.github.com>
2022-10-20 07:47:10 -0300

    Version entregada

---
+++ src/interfaz/Interfaz.java
@@ -0,0 +1,10 @@
+package interfaz;
+
+/** Interfaz es la clase del metodo main. La ejecucion se da en esta clase.
+ * @author yliana*/
+
+public class Interfaz extends Sistema{
+    public static void main(String[] args) {
+        Sistema.menuInicial();
+    }
+}
