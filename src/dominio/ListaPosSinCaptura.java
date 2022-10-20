a3f369c911717635bc89044bb9bf9d110b030a61
Yliana <66150659+YlianaOtero@users.noreply.github.com>
2022-10-20 07:43:29 -0300

    Version entregada

--- src/dominio/ListaPosSinCaptura.java
+++ src/dominio/ListaPosSinCaptura.java
@@ -1,49 +1,25 @@
-/* ListaPosSinCaptura se encarga de la lógica de los movimientos sin captura del
- * juego. Un objeto de esta clase tiene una posición a partir de la cual se genera
- * la lista de posiciones a las cuales se podría desplazar sin captura, siempre
- * dentro de un mismo tablero dado.*/
 package dominio;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author ylian
- */
-public class ListaPosSinCaptura {
+/** Partiendo de la posicion Inicio almancenada en la superclase ListaPosPosibels, 
+ * dentro del tablero incluido en la misma clase, ListaPosSinCaptura se encarga 
+ * de listar las posiciones a las cuales la ficha en Inicio podria desplazarse
+ * sin captura.
+ * @author yliana */
 
-    private Posicion inicio;
-    private Tablero tablero;
+public class ListaPosSinCaptura extends ListaPosPosibles{
+
     private ArrayList<Posicion> lista;
 
     /*CONSTRUCTORES*/
-    public ListaPosSinCaptura(Posicion unaPosicion, Tablero unTablero) {
-        this.inicio = unaPosicion;
-        this.tablero = unTablero;
+    public ListaPosSinCaptura() {
         this.lista = listarPosiciones();
     }
     
     /*METODOS DE ACCESO*/
     public ArrayList<Posicion> getLista() {
         return this.lista;
-    }
-    
-    /*PREDICADOS*/
-    public boolean hayMovimientos() {
-        return !this.lista.isEmpty();
-    }
-    
-    public boolean estaEnLaLista(Posicion pos) {
-        boolean esta = false;
-        
-        for (int i = 0; i < this.lista.size() && !esta; i++) {
-            Posicion actual = this.lista.get(i);
-            if (actual.getFila() == pos.getFila() && actual.getCol() == pos.getCol()) {
-                esta = true;
-            }
-        }
-        
-        return esta;
     }
 
     /*METODOS DE MODIFICACION*/
@@ -55,29 +31,23 @@ public class ListaPosSinCaptura {
         return posiciones;
     }
 
-    /*METODOS AUXILIARES*/
-    protected boolean puedeDesplazarse(Posicion fin) {
-        return fin.getFichaEnPos(this.tablero) == ' ' && 
-                    inicio.getDistanciaAlCentro() < fin.getDistanciaAlCentro();
-    } 
-    
-    
+    /*METODOS PARA LISTAR LAS POSICIONES SEGUN DIRECCION*/
     private ArrayList<Posicion> listarPosHorizontal() {
-        int fila = this.inicio.getFila();
-        int col = this.inicio.getCol();
+        int fila = super.Inicio.getFila();
+        int col = super.Inicio.getCol();
 
         ArrayList<Posicion> posiciones = new ArrayList<Posicion>();
 
         if (col > 0) {
             Posicion nueva = new Posicion(fila, col - 1);
-            if (puedeDesplazarse (nueva)) {
+            if (Posicion.puedeDesplazarse(super.Inicio, nueva, super.Tablero)) {
                 posiciones.add(nueva);
             } 
         }
 
-        if (col < this.tablero.getTamanio() - 1) {
+        if (col < super.Tablero.getTamanio() - 1) {
             Posicion nueva = new Posicion(fila, col + 1);
-            if (puedeDesplazarse (nueva)) {
+            if (Posicion.puedeDesplazarse(super.Inicio, nueva, super.Tablero)) {
                 posiciones.add(nueva);
             }
         }
@@ -86,21 +56,21 @@ public class ListaPosSinCaptura {
     }
 
     private ArrayList<Posicion> listarPosVertical() {
-        int fila = this.inicio.getFila();
-        int col = this.inicio.getCol();
+        int fila = super.Inicio.getFila();
+        int col = super.Inicio.getCol();
 
         ArrayList<Posicion> posiciones = new ArrayList<Posicion>();
 
         if (fila > 0) {
             Posicion nueva = new Posicion(fila - 1, col);
-            if (puedeDesplazarse (nueva)) {
+            if (Posicion.puedeDesplazarse(super.Inicio, nueva, super.Tablero)) {
                 posiciones.add(nueva);
             }
         }
         
-        if (fila < this.tablero.getTamanio() - 1) {
+        if (fila < super.Tablero.getTamanio() - 1) {
             Posicion nueva = new Posicion(fila + 1, col);
-            if (puedeDesplazarse (nueva)) {
+            if (Posicion.puedeDesplazarse(super.Inicio, nueva, super.Tablero)) {
                 posiciones.add(nueva);
             }
         }
@@ -109,36 +79,36 @@ public class ListaPosSinCaptura {
     }
 
     private ArrayList<Posicion> listarPosDiagonal() {
-        int fila = this.inicio.getFila();
-        int col = this.inicio.getCol();
+        int fila = super.Inicio.getFila();
+        int col = super.Inicio.getCol();
 
         ArrayList<Posicion> posiciones = new ArrayList<Posicion>();
 
         if (fila > 0) {
             if (col > 0) {
                 Posicion nueva = new Posicion(fila - 1, col - 1);
-                if (puedeDesplazarse (nueva)) {
+                if (Posicion.puedeDesplazarse(super.Inicio, nueva, super.Tablero)) {
                     posiciones.add(nueva);
                 }
             }
-            if (col < this.tablero.getTamanio() - 1) {
+            if (col < super.Tablero.getTamanio() - 1) {
                 Posicion nueva = new Posicion(fila - 1, col + 1);
-                if (puedeDesplazarse (nueva)) {
+                if (Posicion.puedeDesplazarse(super.Inicio, nueva, super.Tablero)) {
                     posiciones.add(nueva);
                 }
             }
         }
 
-        if (fila < this.tablero.getTamanio() - 1) {
+        if (fila < super.Tablero.getTamanio() - 1) {
             if (col > 0) {
                 Posicion nueva = new Posicion(fila + 1, col - 1);
-                if (puedeDesplazarse (nueva)) {
+                if (Posicion.puedeDesplazarse(super.Inicio, nueva, super.Tablero)) {
                     posiciones.add(nueva);
                 }
             }
-            if (col < this.tablero.getTamanio() - 1) {
+            if (col < super.Tablero.getTamanio() - 1) {
                 Posicion nueva = new Posicion(fila + 1, col + 1);
-                if (puedeDesplazarse (nueva)) {
+                if (Posicion.puedeDesplazarse(super.Inicio, nueva, super.Tablero)) {
                     posiciones.add(nueva);
                 }
             }
