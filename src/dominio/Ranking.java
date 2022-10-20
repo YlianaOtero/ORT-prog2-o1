e4909cb227d097fee42d4ed3917c399a79a7f6b3
Yliana <66150659+YlianaOtero@users.noreply.github.com>
2022-10-20 07:42:01 -0300

    Version entregada

--- src/dominio/Ranking.java
+++ src/dominio/Ranking.java
@@ -1,40 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package dominio;
 
-/**
- *
- * @author nalux
- */
+/** Un Ranking es una lista de jugadores ordenada descendentemente segun las 
+ * victorias. 
+ * @author natalia*/
 public class Ranking{
-        private ListaJugadores listaJugadores;
-        /*CONSTRUCTORES*/
-        public Ranking(ListaJugadores unaListaJugadores){
-            unaListaJugadores.ordenarLista();    
-            this.setListaJugadores = unaListaJugadores;
-        }
-        
-        /*METODOS DE ACCESO*/
-        public String getListaJugadores(){
-                return this.listaJugadores;
-        }
-        
-        /*METODOS DE MODIFICACION*/
-        public void setListaJugadores(ListaJugadores unaListaJugadores){
-                this.listaJugadores = unaListaJugadores;
-        }
-        
-         /*OVERRIDES*/
-          @Override
-          public String toString(){
-            String jugadores = "ALIAS          PARTIDAS JUGADAS             VICTORIAS";
-            for(int i = 0; i < listaJugadores.size(); i++){
-                jugadores += (i + 1) + ". " + listaJugadores.get(i).getAlias() + "          " 
-                        + listaJugadores.get(i).getPartidas() + "           "
-                        +   listaJugadores.get(i).getVictorias() + "\n";
-            }
-            return jugadores;
-          }
+    private ListaJugadores listaJugadores;
+
+    /*CONSTRUCTORES*/
+    public Ranking(ListaJugadores unaListaJugadores){
+        unaListaJugadores.ordenarLista();    
+        this.listaJugadores = unaListaJugadores;
     }
+
+    /*METODOS DE ACCESO*/
+    public ListaJugadores getListaJugadores(){
+        return this.listaJugadores;
+    }
+
+    /*METODOS DE MODIFICACION*/
+    public void setListaJugadores(ListaJugadores unaListaJugadores){
+        this.listaJugadores = unaListaJugadores;
+    }
+
+    /*OVERRIDES*/
+    @Override
+    public String toString(){
+        String pad = listaJugadores.paddingAlias();
+        String jugadores = "************************************************\n"
+                + "                       RANKING                    \n"
+                + "************************************************\n"
+                + "ALIAS " +pad + pad + " PARTIDAS " +pad + pad +" VICTORIAS \n";
+        for(int i = 0; i < listaJugadores.largo(); i++){
+            jugadores += (i + 1) + ". " + listaJugadores.jugadorEnPos(i).getAlias() + pad 
+                    + "          " + listaJugadores.jugadorEnPos(i).getPartidas() + pad
+                    + "           " + listaJugadores.jugadorEnPos(i).getVictorias() + "\n";
+        }
+
+        jugadores += "************************************************\n";
+
+        return jugadores;
+    }
+}
